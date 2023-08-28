27dfed72c44f4a6beaf20d756b8a22df2f98cb68
AngelBrambila91 <malthael.91@gmail.com>
2023-08-28 06:45:18 -0600

    Added Projects

---
+++ 1P/Arguments/Program.cs
@@ -0,0 +1,55 @@
+﻿// string[] args
+WriteLine($"There are {args.Length} arguments.");
+
+foreach (var argument in args)
+{
+    WriteLine(argument);
+}
+
+#region Infer Types
+// PROS : Best type to transfer data
+// CONS : Don't inherit all the methods and/or properties
+object number = "13";
+WriteLine(number.GetType());
+string name = "Nebai";
+
+// PROS : Inherit methods and properties
+// CONS : Allocates memory on CPU and do not use Garbage Collector
+dynamic decimalNumber = 45.8;
+WriteLine(decimalNumber.GetType());
+
+// PROS : Inherit methods and properties
+// PROS : Frees memory from RAM
+var array = new int [1,2,3,4];
+WriteLine(array.GetType());
+#endregion
+
+
+if(args.Length < 3)
+{
+    WriteLine("You must specify two colors and cursor size e.g ");
+    WriteLine("dotnet run red yellow 50");
+    return; // stop running;
+}
+// ambit scope
+// 1st Color
+ForegroundColor = (ConsoleColor)Enum.Parse(
+    enumType: typeof(ConsoleColor),
+    value: args[0],
+    ignoreCase: true
+);
+
+// 2nd Color
+BackgroundColor = (ConsoleColor)Enum.Parse(
+    enumType: typeof(ConsoleColor),
+    value: args[1],
+    ignoreCase: true
+);
+try
+{
+CursorSize = int.Parse(args[2]);
+}
+catch (System.PlatformNotSupportedException)
+{
+    WriteLine("The current platform does not support the change in cursor size");
+}
---
+++ 1P/Formatting/Program.cs
@@ -0,0 +1,33 @@
+﻿using static System.Console;
+// Margins
+// {index [,alignment] [:formatString]}
+string pearText = "Pear";
+int pearCount = 12345;
+string pricklyPearText = "Prickly Pear";
+int pricklyPearCount = 450000;
+
+// HEADERS
+WriteLine(
+format: "{0, -12} {1, 7}",
+arg0: "Name",
+arg1: "Count");
+
+// 1st Row
+WriteLine(
+    format:"{0,-12} {1,7:N0}",
+    arg0: pearText,
+    arg1: pearCount
+);
+
+// 2nd Row
+WriteLine(
+    format:"{0,-12} {1,7:N0}",
+    arg0: pricklyPearText,
+    arg1: pricklyPearCount
+);
+
+Write("Type your first name and press ENTER : ");
+string firstName = ReadLine();
+Write("Type your age and press ENTER : ");
+string age = ReadLine();
+WriteLine($"Hello {firstName} you look good for {age}");
