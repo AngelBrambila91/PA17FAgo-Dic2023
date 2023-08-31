5ebc935d1b0f46c0c85de6b9ffb345375f86dac5
AngelBrambila91 <malthael.91@gmail.com>
2023-08-31 08:17:09 -0600

    Finished Control Statements, added Casting project

---
+++ 1P/CastingAndConverting/Program.cs
@@ -0,0 +1,46 @@
+﻿using static System.Convert;
+// 2 types of conversion
+// Implicit
+// Explicit
+
+
+#region Implicit Convertion
+int a = 10;
+double b = a;
+WriteLine($"b is double and is {b}");
+#endregion
+
+#region Explicit Convertion
+    double c = 9.8;
+    int d = (int)c;
+    WriteLine($"d is int and its {d}");
+
+    long e = 10;
+    int f = (int)e;
+    WriteLine($"e is {e:N0} and f is {f:N0}");
+    e = long.MaxValue;
+    f = (int)e;
+    WriteLine($"e is {e:N0} and f is {f:N0}");
+    e = 5_000_000_000;
+    f = (int)e;
+    WriteLine($"e is {e:N0} and f is {f:N0}");
+#endregion
+
+#region using Convert Class
+    double g = 9.8;
+    int h = ToInt32(g);
+    WriteLine($"g is {g} and h is {h}");
+#endregion
+
+#region toString
+    int number = 12;
+    WriteLine(number.ToString());
+    bool boolean = true;
+    WriteLine(boolean.ToString());
+    DateTime now = DateTime.Now;
+    WriteLine(now.ToString());
+    string name = "Nebai";
+    WriteLine(name.ToString());
+    object something =  new();
+    WriteLine(something.ToString());
+#endregion
--- 1P/ConditionalStatements/Program.cs
+++ 1P/ConditionalStatements/Program.cs
@@ -8,7 +8,7 @@
 // }
 
 string password = "SatouroGoujo";
-if(password .Length < 12)
+if (password.Length < 12)
 {
     WriteLine("Your password is too short. Use at least 12");
 }
@@ -53,3 +53,62 @@ WriteLine("After end of switch");
 A_label:
 WriteLine("After A_label");
 
+Animal?[] animals = new Animal?[]
+{
+        new Pig {
+            Name = "Oreo",
+            Born = new (year: 2012, month: 8, day: 2),
+            Legs = 4,
+            IsDomestic = true,
+            Age = 12
+        },
+        new Cat {
+            Name = "Juno",
+            Born = new (year: 2010, month: 4, day: 23),
+            Legs = 4,
+            IsDomestic = true,
+            Age = 12
+        },
+
+        new Dog {
+            Name = "Platon",
+            Born = new (year: 2012, month: 8, day: 2),
+            Legs = 3,
+            IsDomestic = true,
+            Age = 12
+        }
+
+};
+// pattern match switch
+foreach (var animal in animals)
+{
+    string message = String.Empty;
+    switch (animal)
+    {
+        case Cat fourLegCat when fourLegCat.Legs == 4:
+        message = $"The name of the cat is {fourLegCat.Name}";
+        break;
+        
+        case Pig chanchito when !chanchito.IsDomestic:
+        message = $"You have a beautiful companion mate, say hi to {chanchito.Name}";
+        break;
+
+        case Dog platon when platon.Age == 11:
+        message = $"Oldie but goldie, good old {platon.Name}";
+        break;
+        
+        default:
+        message = "That type of animal does not exists.";
+        break;
+    }
+
+    // simplified switch | minimalist switch
+    message = animal switch
+    {
+        // => Lambda
+        Cat threeLegCat when threeLegCat.Legs == 3 => 
+        $"The name of the three leg cat is {threeLegCat.Name}",
+        Cat poisonousCat when poisonousCat.isPoisonous =>
+        "STAY AWAY from that michi mate"
+    };
+}
