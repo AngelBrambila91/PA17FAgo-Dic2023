2d71dc23dfe914cc804bafbf5f5df12f58dd398d
AngelBrambila91 <malthael.91@gmail.com>
2023-09-18 10:57:50 -0600

    Added PeopleApp and PeopleLibraryProject

---
+++ 2P/Libraries/Person.cs
@@ -0,0 +1,33 @@
+﻿namespace PA17F.Shared;
+
+public class Person
+{
+    // members
+    // constant, read -onlys, events
+    /* Constant : Once declared and 
+    assigned, cannot be changed even
+    in runtime
+
+    Read-Only : Similar to a constant
+    but you can change the value on runtime
+    using constructor
+
+    Event: Reference to another method
+
+    Methods: Functions, Execute statements
+    Constructor : Executed when the NEW , allocates memory
+    
+    Property : 
+    member -> int number = 0;
+    public void setNumber (int number ) { this.number = number; }
+    public int get Number() { return this.number; }
+    string Name { get; set;} getter and setters
+
+    Indexers: []
+    Operators : + - => * / &
+    */
+    
+    // members
+    public string? Name;
+    public DateTime DateOfBirth;
+}
---
+++ 2P/OOP/PeopleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿using PA17F.Shared;
+
+Person wachi = new();
+WriteLine($"Wachi is : {wachi.ToString()}");
+
+wachi.Name = "Alberto";
+wachi.DateOfBirth = new DateTime(2002, 12, 22);
+WriteLine(format: "{0} was born on {1:dddd, d MMMM yyyy}", 
+arg0: wachi.Name,
+arg1: wachi.DateOfBirth);
